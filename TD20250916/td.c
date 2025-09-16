787d19971db81260ffd6cf211f2bc422a8dd8960
Bignensdamien <damien.bignens@heig-vd.ch>
2025-09-16 12:32:49 +0000

    added TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23; // variabele non signée
+	signed int account_value = -42; // variabele signée 
+	float temperature = 18.1; //32 bit variabele flottante
+	double pi = 3.141592653589793; //64 bit variabele double précision
+	char letter = 'A'; // variabele caractère	
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}
